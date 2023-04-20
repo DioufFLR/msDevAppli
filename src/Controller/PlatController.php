63408b0fa2b9a37aa36c996b0fbbef8fc24fe3a6
DioufFLR <geoffreyfleur@gmail.com>
2023-04-20 16:02:39 +0200

    1.7

--- src/Controller/PlatController.php
+++ src/Controller/PlatController.php
@@ -2,20 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Entity\Plat;
 use App\Repository\PlatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class PlatController extends AbstractController
 {
-    #[Route('/plat', name: 'app_plat')]
+    #[Route('/plat', name: 'app_plat_index')]
     public function index(PlatRepository $platRepository): Response
     {
         $liste = $platRepository->findAll();
 
         return $this->render('plat/index.html.twig', [
             'liste' => $liste
+        ]);
+    }
+
+    #[Route('/detail/{categorie}', name: 'app_plat_detail')]
+    public function detail(Categorie $categorie): Response
+    {
+        return $this->render('plat/index.html.twig', [
+            "categorie" => $categorie
         ]);
     }
 }
