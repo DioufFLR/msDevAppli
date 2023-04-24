749864ed5e3a6a7afcf9372f09a53ffeb50d37b4
DioufFLR <geoffreyfleur@gmail.com>
2023-04-24 11:12:39 +0200

    2.1 pagination plat page

--- src/Controller/PlatController.php
+++ src/Controller/PlatController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Categorie;
 use App\Entity\Plat;
 use App\Repository\PlatRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +15,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class PlatController extends AbstractController
 {
     #[Route('/plat', name: 'app_plat_index')]
-    public function index(PlatRepository $platRepository): Response
+    public function index(PlatRepository $platRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        $liste = $platRepository->findAll();
+        $liste = $paginator->paginate(
+            $platRepository->findAll(),
+            $request->query->getInt('page', 1), /*page number*/
+            3 /*limit per page*/
+        );
+
 
         return $this->render('plat/index.html.twig', [
             'liste' => $liste
