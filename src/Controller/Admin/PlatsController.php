9d792bbed921acd90c58519b4cbfd8be01922715
DioufFLR <geoffreyfleur@gmail.com>
2023-04-25 15:37:45 +0200

    2.5 CRUD for edit and add

--- src/Controller/Admin/PlatsController.php
+++ src/Controller/Admin/PlatsController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Plat;
 use App\Form\PlatsFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,7 +21,7 @@ class PlatsController extends AbstractController
 
     // Ajouter un plat
     #[Route('/ajout', name: 'add')]
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
@@ -29,15 +31,53 @@ class PlatsController extends AbstractController
         // On crée le formulaire
         $platForm = $this->createForm(PlatsFormType::class, $plat);
 
+        // On traite la requête du formulaire
+        $platForm->handleRequest($request);
+
+        // On vérifie si le formulaire est soumis et valide
+        if ($platForm->isSubmitted() && $platForm->isValid()){
+
+            // On stock
+            $entityManager->persist($plat);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Plat ajouté avec succès');
+
+            // On redirige
+            return $this->redirectToRoute('admin_plats_index');
+        }
+
         return $this->renderForm('admin/plats/add.html.twig', compact('platForm'));
     }
 
     // Modifier un plat
     #[Route('/edition/{id}', name: 'edit')]
-    public function edit(Plat $plat): Response
+    public function edit(Plat $plat, Request $request, EntityManagerInterface $entityManager): Response
     {
         // On vérifie si l'utilisateur peut éditer avec le Voter
         $this->denyAccessUnlessGranted('PLAT_EDIT', $plat);
+
+        // On crée le formulaire
+        $platForm = $this->createForm(PlatsFormType::class, $plat);
+
+        // On traite la requête du formulaire
+        $platForm->handleRequest($request);
+
+        // On vérifie si le formulaire est soumis et valide
+        if ($platForm->isSubmitted() && $platForm->isValid()){
+
+            // On stock
+            $entityManager->persist($plat);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Plat modifié avec succès');
+
+            // On redirige
+            return $this->redirectToRoute('admin_plats_index');
+        }
+
+        return $this->renderForm('admin/plats/edit.html.twig', compact('platForm'));
+
         return $this->render('admin/plats/index.html.twig');
     }
 
--- src/Form/PlatsFormType.php
+++ src/Form/PlatsFormType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Categorie;
 use App\Entity\Plat;
+use App\Repository\CategorieRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,12 +15,32 @@ class PlatsFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libelle')
-            ->add('description')
-            ->add('prix')
-            ->add('image')
-            ->add('active')
-            ->add('categorie')
+            ->add('libelle', options: [
+                'label' => 'Nom'
+            ])
+            ->add('description', options: [
+                'label' => 'Description'
+            ])
+            ->add('prix', options: [
+                'label' => 'Prix'
+            ])
+            ->add('image', options: [
+                'label' => 'Image'
+            ])
+            ->add('active', options: [
+                'label' => 'Active'
+            ])
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'libelle',
+                'label' => 'Catégorie',
+                'query_builder' => function(CategorieRepository $categorieRepository)
+                {
+                    // Pour trier par ordre alphabétique
+                    return $categorieRepository->createQueryBuilder('c')
+                        ->OrderBy('c.libelle', 'ASC');
+                }
+            ])
         ;
     }
 
