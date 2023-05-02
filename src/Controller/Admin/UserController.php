d236ab9820e4112e790a04b5cb3de784a96d9a55
DioufFLR <geoffreyfleur@gmail.com>
2023-05-02 14:27:36 +0200

    3.1 interface administrator

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -7,13 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/plats', name: 'admin_users_')]
+#[Route('/admin/users', name: 'admin_users_')]
 class UserController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(UtilisateurRepository $utilisateurRepository): Response
     {
-        $user = $utilisateurRepository->findBy([], ['nom' => 'asc']);
+        $user = $utilisateurRepository->findBy([], ['id' => 'asc']);
 
         return $this->render('admin/users/index.html.twig', compact('user'));
     }
